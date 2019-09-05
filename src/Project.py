82bcc6e76faa22a1c58f839835ce593a63e8f204
Ayça Begüm Taşçıoğlu <37643134+aeyc@users.noreply.github.com>
2019-09-05 14:59:10 +0300

    Add files via upload

--- src/Project.py
+++ src/Project.py
@@ -33,7 +33,16 @@ infos = infos.str.replace(']',' ').str.replace(' +',' ').str.strip()
 infos = infos.str.lower()
 for i in infos:
     i= re.sub('[\W_]+', '', str(i))
+df['sentence'] = infos
 df['cleaned'] = [ nltk.word_tokenize( str(sentence) ) for sentence in infos ]
+#%%
+"""
+DROP NAN VALUES
+"""
+df = df.dropna()
+"""
+NAN
+"""
 #%% 
 """
 Data analysis
@@ -101,7 +110,6 @@ Safe Froms
 safe_str = str(safe_forms['cleaned'])
 safe_str = punc_rep(safe_str)
 remove_digits = str.maketrans('','',digits)
-safe_str = safe_str.translate(remove_digits)
 
 split_safe = safe_str.split() 
 safe_dct = dict()
@@ -115,7 +123,7 @@ for i in split_safe:
     if i in safe_dct:
         safe_dct[i] += 1
     else:
-        safe_dct[i] = 1
+        safe_dct[i] = 1 
 #%%
 #Z score
 """
@@ -123,13 +131,15 @@ Dictionary to Dataframe
 
 """
 from scipy import stats
-safe_df = pd.DataFrame.from_dict(safe_dct, orient='index')
-safe_df.columns=['count']
+safe_df = pd.DataFrame(list(safe_dct.items()))
+safe_df.columns=['word','count']
 safe_df['zscore'] = stats.zscore(safe_df['count'])
+safe_df['label'] = 'SAFE'
 
-spam_df = pd.DataFrame.from_dict(spam_dct, orient='index')
-spam_df.columns=['count']
+spam_df = pd.DataFrame(list(spam_dct.items()))
+spam_df.columns=['word','count']
 spam_df['zscore'] = stats.zscore(spam_df['count'])
+spam_df['label'] = 'SPAM'
 
 #%%
 """
@@ -137,8 +147,67 @@ Normalization of data with ZScore
 spam_df>3: email,submit,form,name,password,student - <-3 empty df
 safe_df>3: submit,name,email - <-3 empty df
 """
-spam_df.drop(spam_df[ spam_df['zscore'] >3 ].index , inplace=True)
+spam_df.drop(spam_df[ spam_df['zscore'] >3.35  ].index , inplace=True)
 safe_df.drop(safe_df[ safe_df['zscore'] >3 ].index , inplace=True)
 #%%
+"""
+Plots
+"""
+#import matplotlib.pyplot as plt
+#plt.scatter(safe_df.word, safe_df.zscore)
+#plt.xlabel("word")
+#plt.ylabel("zscore")
+#plt.show()
+#%%
+"""
+Algortihm trial for tdif
+n: total no of words in spam forms
+"""
+#def tf_idf(i,j):
+#   return tf(i,j)*idf(i,j)
+#
+#def tf(i,j):
+#    return i/j
+#
+#def idf(i,j):
+#    return np.log(j/i)
+#
+#n = sum(spam_df['count'])
+#m = sum(safe_df['count'])
+#
+#spam_df['tf_idf'] = tf_idf(spam_df['count'],n)
+#safe_df['tf_idf'] = tf_idf(safe_df['count'],n)
 
+#%%
+"""
+Vectorizing with Tfidf Vectorizer
 
+"""
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import datasets
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+
+new_df = spam_df.append(safe_df)
+X_train, X_test, y_train, y_test = train_test_split(df['sentence'], df['target'], test_size = 0.1, random_state = 1)
+#X_train, X_test, y_train, y_test = train_test_split(new_df['word'].sample(n = 146),spam_df['word'], test_size = 0.1, random_state = 1)
+
+vectorizer = TfidfVectorizer()
+
+X_train = vectorizer.fit_transform(X_train)
+
+svm = svm.SVC(kernel='linear')
+svm.fit(X_train, y_train)
+
+#%%
+from sklearn.metrics import confusion_matrix
+X_test = vectorizer.transform(X_test)
+y_pred = svm.predict(X_test)
+print(confusion_matrix(y_test, y_pred))
+
+def pred(msg):
+    msg = vectorizer.transform([msg])
+    prediction = svm.predict(msg)
+    #svm.predict(vectorizer.transform(['password']))
+    return prediction[0]
